2b56f42059f36f7fc76a315f91c947741650ffe3
zhangkai <zhangkai.gis@163.com>
2024-06-18 16:07:45 +0800

    0 means do not halt sequencer

--- zk/stages/stage_sequence_execute_xlayer.go
+++ zk/stages/stage_sequence_execute_xlayer.go
@@ -8,7 +8,8 @@ import (
 )
 
 func tryHaltSequencer(logPrefix string, cfg SequenceBlockCfg, thisBatch uint64) {
-	if cfg.zk.SequencerHaltOnBatchNumber == thisBatch {
+	if cfg.zk.SequencerHaltOnBatchNumber != 0 &&
+		cfg.zk.SequencerHaltOnBatchNumber == thisBatch {
 		for {
 			log.Info(fmt.Sprintf("[%s] Halt sequencer on batch %d...", logPrefix, thisBatch))
 			time.Sleep(5 * time.Second) //nolint:gomnd

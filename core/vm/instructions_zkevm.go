f5e89813dd0c851b32efc9f6c83999444fd76028
ylsGit <bzwdyls@163.com>
2024-06-26 11:21:10 +0800

    set ValueWei=”“ for delegatecall

--- core/vm/instructions_zkevm.go
+++ core/vm/instructions_zkevm.go
@@ -456,7 +456,8 @@ func opDelegateCall_zkevm(pc *uint64, interpreter *EVMInterpreter, scope *ScopeC
 	innerTx, newIndex := beforeOp(interpreter, DELEGATECALL_TYP, scope.Contract.Address(), &toAddr, nil, args, gas, big.NewInt(0))
 	ret, returnGas, err := interpreter.evm.DelegateCall(scope.Contract, toAddr, args, gas)
 	innerTx.TraceAddress = scope.Contract.CallerAddress.String()
-	innerTx.ValueWei = scope.Contract.value.ToBig().String()
+	//innerTx.ValueWei = scope.Contract.value.ToBig().String()
+	innerTx.ValueWei = "" // for downstream services, DELEGATECALL no transfer of assets actually occurs
 	innerTx.CallValueWei = hexutil.EncodeBig(scope.Contract.value.ToBig())
 	afterOp(interpreter, DELEGATECALL_TYP, gas-returnGas, newIndex, innerTx, nil, err)
 	if err != nil {

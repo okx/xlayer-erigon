b6ac177029f1949d77bc6db435aec38f84578777
ylsGit <bzwdyls@163.com>
2024-07-02 15:12:24 +0800

    Update evm_xlayer.go

--- core/vm/evm_xlayer.go
+++ core/vm/evm_xlayer.go
@@ -19,29 +19,6 @@ const (
 	SUICIDE_TYP      = "suicide"
 )
 
-//// InnerTx stores the basic field of an inner tx.
-//// NOTE: DON'T change this struct for:
-//// 1. It will be written to database, and must be keep the same type When reading history data from db
-//// 2. It will be returned by rpc method
-//type InnerTx struct {
-//	Dept          big.Int `json:"dept"`
-//	InternalIndex big.Int `json:"internal_index"`
-//	CallType      string  `json:"call_type"`
-//	Name          string  `json:"name"`
-//	TraceAddress  string  `json:"trace_address"`
-//	CodeAddress   string  `json:"code_address"`
-//	From          string  `json:"from"`
-//	To            string  `json:"to"`
-//	Input         string  `json:"input"`
-//	Output        string  `json:"output"`
-//	IsError       bool    `json:"is_error"`
-//	Gas           uint64  `json:"gas"`
-//	GasUsed       uint64  `json:"gas_used"`
-//	Value         string  `json:"value"`
-//	ValueWei      string  `json:"value_wei"`
-//	Error         string  `json:"error"`
-//}
-
 type InnerTxMeta struct {
 	index     int
 	lastDepth int
@@ -129,7 +106,7 @@ func afterOp(interpreter *EVMInterpreter, opType string, gas_used uint64, newInd
 	}
 
 	switch opType {
-	case "create":
+	case CREATE_TYP, CREATE2_TYP:
 		innerTx.To = addr.String()
 	}
 }

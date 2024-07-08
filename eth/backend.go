00b30073dfd9a5d13b174458f689b2752eb2a36a
chengzhinei <chengxue5yuan@163.com>
2024-07-08 17:33:44 +0800

    add gas price cache (#46)
    Co-authored-by: LeoGuo621 <335209779@qq.com>
    Co-authored-by: Niven <sieniven@gmail.com>
    Co-authored-by: oker <oker@okerdeMacBook-Pro.local>

--- eth/backend.go
+++ eth/backend.go
@@ -1023,7 +1023,9 @@ func (backend *Ethereum) Init(stack *node.Node, config *ethconfig.Config) error 
 	if casted, ok := backend.engine.(*bor.Bor); ok {
 		borDb = casted.DB
 	}
-	apiList := commands.APIList(chainKv, borDb, ethRpcClient, txPoolRpcClient, miningRpcClient, ff, stateCache, blockReader, backend.agg, httpRpcCfg, backend.engine, config, backend.l1Syncer)
+	apiList, gpCache := commands.APIList(chainKv, borDb, ethRpcClient, txPoolRpcClient, miningRpcClient, ff, stateCache, blockReader, backend.agg, httpRpcCfg, backend.engine, config, backend.l1Syncer)
+	// For X Layer
+	backend.txPool2.SetGpCacheForXLayer(gpCache)
 	authApiList := commands.AuthAPIList(chainKv, ethRpcClient, txPoolRpcClient, miningRpcClient, ff, stateCache, blockReader, backend.agg, httpRpcCfg, backend.engine, config)
 	go func() {
 		if err := cli.StartRpcServer(ctx, httpRpcCfg, apiList, authApiList); err != nil {

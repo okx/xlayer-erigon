d14ca7ff2627dbfbecced0169456db4b2dd86373
chengzhinei <chengxue5yuan@163.com>
2024-07-16 11:18:12 +0800

    fix default

--- cmd/utils/flags_xlayer.go
+++ cmd/utils/flags_xlayer.go
@@ -258,9 +258,9 @@ func setTxPoolXLayer(ctx *cli.Context, cfg *ethconfig.DeprecatedTxPoolConfig) {
 			sender := libcommon.HexToAddress(senderHex)
 			cfg.FreeClaimGasAddr[i] = sender.String()
 		}
-		if len(cfg.FreeClaimGasAddr) == 0 {
-			cfg.FreeClaimGasAddr = []string{"0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"}
-		}
+	}
+	if len(cfg.FreeClaimGasAddr) == 0 {
+		cfg.FreeClaimGasAddr = []string{"0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"}
 	}
 	if ctx.IsSet(TxPoolGasPriceMultiple.Name) {
 		cfg.GasPriceMultiple = ctx.Uint64(TxPoolGasPriceMultiple.Name)

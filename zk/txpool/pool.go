2f4104db8900432e6f6cba17ffda40c3327dacd6
Niven <sieniven@gmail.com>
2024-08-02 13:56:01 +0800

    Refactor and set default pool configs (#93)

--- zk/txpool/pool.go
+++ zk/txpool/pool.go
@@ -24,7 +24,6 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	"github.com/ledgerwatch/erigon/rlp"
 	"math"
 	"math/big"
 	"runtime"
@@ -32,6 +31,8 @@ import (
 	"sync"
 	"sync/atomic"
 	"time"
+
+	"github.com/ledgerwatch/erigon/rlp"
 
 	"github.com/VictoriaMetrics/metrics"
 	mapset "github.com/deckarep/golang-set/v2"
@@ -329,11 +330,9 @@ type TxPool struct {
 	ethCfg                  *ethconfig.Config
 	aclDB                   kv.RwDB
 
-	wbCfg WBConfig // XLayer config
-
 	// For X Layer
-	// gpCache will only work in sequencer node, without rpc node
-	gpCache GPCache
+	xlayerCfg XLayerConfig
+	gpCache   GPCache // GPCache will only work in sequencer node, without rpc node
 
 	// we cannot be in a flushing state whilst getting transactions from the pool, so we have this mutex which is
 	// exposed publicly so anything wanting to get "best" transactions can ensure a flush isn't happening and
@@ -388,12 +387,13 @@ func New(newTxs chan types.Announcements, coreDB kv.RoDB, cfg txpoolcfg.Config, 
 		flushMtx:                &sync.Mutex{},
 		aclDB:                   aclDB,
 		limbo:                   newLimbo(),
-		wbCfg: WBConfig{ // XLayer config
-			EnableWhitelist:  ethCfg.DeprecatedTxPool.EnableWhitelist,
-			WhiteList:        ethCfg.DeprecatedTxPool.WhiteList,
-			BlockedList:      ethCfg.DeprecatedTxPool.BlockedList,
-			FreeClaimGasAddr: ethCfg.DeprecatedTxPool.FreeClaimGasAddr,
-			GasPriceMultiple: ethCfg.DeprecatedTxPool.GasPriceMultiple,
+		// X Layer config
+		xlayerCfg: XLayerConfig{
+			EnableWhitelist:   ethCfg.DeprecatedTxPool.EnableWhitelist,
+			WhiteList:         ethCfg.DeprecatedTxPool.WhiteList,
+			BlockedList:       ethCfg.DeprecatedTxPool.BlockedList,
+			FreeClaimGasAddrs: ethCfg.DeprecatedTxPool.FreeClaimGasAddrs,
+			GasPriceMultiple:  ethCfg.DeprecatedTxPool.GasPriceMultiple,
 		},
 	}, nil
 }
@@ -806,7 +806,7 @@ func (p *TxPool) validateTx(txn *types.TxSlot, isLocal bool, stateCache kvcache.
 	}
 
 	// X Layer check if sender is whitelisted
-	if p.wbCfg.EnableWhitelist && !p.checkWhiteAddr(from) {
+	if p.xlayerCfg.EnableWhitelist && !p.checkWhiteAddr(from) {
 		log.Info(fmt.Sprintf("TX TRACING: validateTx sender is not whitelisted idHash=%x, txn.sender=%s", txn.IDHash, from))
 		return NoWhiteListedSender
 	}

67403be02a0d2d0414b61139aff1f74e92c3f10d
LeoGuo621 <335209779@qq.com>
2024-08-16 11:26:37 +0800

    Optimize eth_minGasPrice

--- cmd/rpcdaemon/commands/eth_api_xlayer.go
+++ cmd/rpcdaemon/commands/eth_api_xlayer.go
@@ -23,6 +23,11 @@ func (apii *APIImpl) runL2GasPricerForXLayer() {
 // cacheSize = 300sec (TTL) / 10sec (UpdatePeriod) = 30
 const cacheSize = 30 // Circular buffer size
 
+// minGPWindowSize defines the number of recent raw GPs to consider
+// when calculating the MinGP from the circular buffer.
+// This value represents the window size within the buffer.
+const minGPWindowSize = 27
+
 type RawGPCache struct {
 	values [cacheSize]*big.Int // Circular buffer
 	head   int                 // Points to the current head of the buffer
@@ -47,6 +52,33 @@ func (c *RawGPCache) GetMin() (*big.Int, error) {
 	isEmpty := true
 	minRGP := big.NewInt(0).SetInt64(math.MaxInt64) // Initialize to maximum big.Int
 	for _, value := range c.values {
+		if value == nil {
+			continue
+		}
+		isEmpty = false
+		if value.Cmp(minRGP) < 0 {
+			minRGP = value
+		}
+	}
+
+	if isEmpty {
+		return nil, fmt.Errorf("no values in cache")
+	}
+
+	return new(big.Int).Set(minRGP), nil
+}
+
+// GetMinGPMoreRecent returns the minimum RGP in the circular buffer for the last minGPWindowSize elements
+func (c *RawGPCache) GetMinGPMoreRecent() (*big.Int, error) {
+	isEmpty := true
+	minRGP := big.NewInt(0).SetInt64(math.MaxInt64) // Initialize to maximum big.Int
+
+	// Calculate the start index for the last minGPWindowSize elements
+	start := (c.head - minGPWindowSize + cacheSize) % cacheSize
+
+	for i := 0; i < minGPWindowSize; i++ {
+		index := (start + i) % cacheSize
+		value := c.values[index]
 		if value == nil {
 			continue
 		}

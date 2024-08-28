7b25e3021a3e8c4fe5526822d55e0d9b769f6bed
jianguo <zjg555543@163.com>
2024-08-28 16:29:52 +0800

    update

--- zk/da/client.go
+++ zk/da/client.go
@@ -3,26 +3,42 @@ package da
 import (
 	"context"
 	"fmt"
+	"io"
 	"net/http"
 	"strings"
 	"time"
 
 	"github.com/gateway-fm/cdk-erigon-lib/common"
 	"github.com/ledgerwatch/erigon/common/hexutil"
+	"github.com/ledgerwatch/log/v3"
 
 	"github.com/ledgerwatch/erigon/zkevm/jsonrpc/client"
 )
 
-const maxAttempts = 10
-const retryDelay = 500 * time.Millisecond
+const maxAttempts = 60
+const retryDelay = 1000 * time.Millisecond
 
 func GetOffChainData(ctx context.Context, url string, hash common.Hash) ([]byte, error) {
 	attemp := 0
 
 	for attemp < maxAttempts {
+		// X Layer for timeout
+		http.DefaultClient.Timeout = 30 * time.Second
+		transport := &http.Transport{
+			DisableKeepAlives: false,
+		}
+		http.DefaultClient.Transport = transport
 		response, err := client.JSONRPCCall(url, "sync_getOffChainData", hash)
 
 		if httpErr, ok := err.(*client.HTTPError); ok && httpErr.StatusCode == http.StatusTooManyRequests {
+			log.Error(fmt.Sprintf("GetOffChainData StatusTooManyRequests， hash:%v, attemp:%v, error:%v", hash.String(), attemp, err))
+			time.Sleep(retryDelay)
+			attemp += 1
+			continue
+		}
+
+		if err == io.EOF {
+			log.Error(fmt.Sprintf("GetOffChainData io.EOF， hash:%v, attemp:%v, error:%v", hash.String(), attemp, err))
 			time.Sleep(retryDelay)
 			attemp += 1
 			continue

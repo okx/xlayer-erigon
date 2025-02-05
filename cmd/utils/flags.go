3ab636b70b3700c0dc1b0f2aae162014634b182f
Scott Fairclough <70711990+hexoscott@users.noreply.github.com>
2025-02-05 21:07:28 +0000

    2.61 deadlocks (#1713)
    * pool to manage flush locking internally
    
    * acquire semaphore before tracking tx begin
    
    * set no gossip to default to true
    
    This really isn't needed in zkevm networks as we don't support p2p gossip
    and it creates load on the DB that we simply don't need.
    
    Some of the deadlocks we've seen, once released, log errors from the
    gossip code on repeat so better we don't attempt it at all in this case
    
    Setting the flag to false will override this default so we can re-enable
    when the time suits
    
    * do not lock on waiting for the open transactions to close
    
    if we lock here then we can't close down already open transactions
    because they will deadlock as they enter the function
    
    * update to mdbx deadlock code test
    
    allows the test to run to completion without thread exhaustion
    
    * useful comment about the stacked locks in the pool best function
    
    * enable pool p2p gossip for unit tests that rely on the old default behaviour
    
    * remove unnecessary 2nd RO tx in send transaction
    
    * do not call track end on error acquiring the semaphore
    
    * increase timeout in stream client tests

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -2396,6 +2396,8 @@ func SetEthConfig(ctx *cli.Context, nodeConfig *nodecfg.Config, cfg *ethconfig.C
 
 	if ctx.IsSet(TxPoolGossipDisableFlag.Name) {
 		cfg.DisableTxPoolGossip = ctx.Bool(TxPoolGossipDisableFlag.Name)
+	} else {
+		cfg.DisableTxPoolGossip = txpoolcfg.DefaultConfig.NoGossip
 	}
 }
 

74366c0dd17f0757680b88f98d0964efbdc92de5
KyrinCode <kyrincode@qq.com>
2025-02-10 11:49:36 +0800

    add worker pool to ReadBatchesWithConcurrency

--- zk/datastream/server/data_stream_server.go
+++ zk/datastream/server/data_stream_server.go
@@ -719,7 +719,6 @@ LOOP_ENTRIES:
 
 func (srv *ZkEVMDataStreamServer) ReadBatchesWithConcurrency(start uint64, end uint64) ([][]*types.FullL2Block, error) {
 	batches := make([][]*types.FullL2Block, end-start+1)
-
 	batchPositions := make([]uint64, end-start+1)
 	var wg sync.WaitGroup
 	errCh := make(chan error, end-start+1)
@@ -750,52 +749,59 @@ func (srv *ZkEVMDataStreamServer) ReadBatchesWithConcurrency(start uint64, end u
 	default:
 	}
 
+	// worker pool
+	maxWorkers := 16
+	jobs := make(chan uint64, end-start+1)
 	wg = sync.WaitGroup{}
-	for i := start; i <= end; i++ {
+
+	for i := 0; i < maxWorkers; i++ {
 		wg.Add(1)
-		go func(batchNum uint64) {
+		go func() {
 			defer wg.Done()
-
-			iterator := newDataStreamServerIterator(srv.streamServer, batchPositions[batchNum-start])
-			blocks := []*types.FullL2Block{}
-		LOOP_ENTRIES:
-			for {
-				parsedProto, _, err := client.ReadParsedProto(iterator)
-				if err != nil {
-					errCh <- err
-					return
-				}
-
-				if parsedProto == nil {
-					break
-				}
-
-				switch parsedProto := parsedProto.(type) {
-				case *types.BatchStart:
-					continue
-				case *types.BatchEnd:
-					if parsedProto.Number == batchNum {
-						break LOOP_ENTRIES
+			for batchNum := range jobs {
+				iterator := newDataStreamServerIterator(srv.streamServer, batchPositions[batchNum-start])
+				blocks := []*types.FullL2Block{}
+			LOOP_ENTRIES:
+				for {
+					parsedProto, _, err := client.ReadParsedProto(iterator)
+					if err != nil {
+						errCh <- err
+						return
 					}
-				case *types.FullL2Block:
-					if parsedProto.BatchNumber == batchNum {
-						blocks = append(blocks, parsedProto)
+					if parsedProto == nil {
+						break
 					}
-				default:
-					continue
+					switch parsedProto := parsedProto.(type) {
+					case *types.BatchStart:
+						continue
+					case *types.BatchEnd:
+						if parsedProto.Number == batchNum {
+							break LOOP_ENTRIES
+						}
+					case *types.FullL2Block:
+						if parsedProto.BatchNumber == batchNum {
+							blocks = append(blocks, parsedProto)
+						}
+					default:
+						continue
+					}
 				}
+				batches[batchNum-start] = blocks
 			}
-
-			batches[batchNum-start] = blocks
-		}(i)
+		}()
 	}
+
+	for i := start; i <= end; i++ {
+		select {
+		case err := <-errCh:
+			close(jobs)
+			return nil, err
+		default:
+			jobs <- i
+		}
+	}
+	close(jobs)
 	wg.Wait()
-
-	select {
-	case err := <-errCh:
-		return nil, err
-	default:
-	}
 
 	return batches, nil
 }
